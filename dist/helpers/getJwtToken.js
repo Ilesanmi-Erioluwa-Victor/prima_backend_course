a0b082d43d07b3fc444e5661efbc9aa12637ed7f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-17 22:26:07 +0100

    Code working as expected

--- dist/helpers/getJwtToken.js
+++ dist/helpers/getJwtToken.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getJwtToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const getJwtToken = (userId) => {
-    return jsonwebtoken_1.default.sign(userId, process.env.);
+    return jsonwebtoken_1.default.sign(userId, `${process.env.JWT_SECRET}`, { expiresIn: "30d" });
 };
 exports.getJwtToken = getJwtToken;
--- src/helpers/getJwtToken.ts
+++ src/helpers/getJwtToken.ts
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const getJwtToken = (userId: string): string => {
-  return jwt.sign(userId, process.env.);
+  return jwt.sign(userId, `${process.env.JWT_SECRET}`, {expiresIn : "30d"});
 };
