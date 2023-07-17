e8fabc1ba484f641c39ceeb84d74e33ee831bc62
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-17 21:53:10 +0100

    Code working as expected

--- dist/server.js
+++ dist/server.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.get("/", (req, res, next) => {
-    res.send("Hi from youtube live...");
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get('/', (req, res, next) => {
+    res.send('Hi from youtube live...');
 });
 app.listen(5000, () => {
-    console.log("Server running on port 5000");
+    console.log('Server running on port 5000');
 });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
-import cookieParser from "cookie-parser"
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-    res.send("Hi from youtube live...")
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res, next) => {
+  res.send('Hi from youtube live...');
+});
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000")
-})
+  console.log('Server running on port 5000');
+});
