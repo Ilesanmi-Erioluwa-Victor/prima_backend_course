a54b3ce221ea31889541bb90ba6e7b4960941f1a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-17 23:10:06 +0100

    Code working as expected

--- dist/utils/cookieToken.js
+++ dist/utils/cookieToken.js
@@ -3,4 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const getJwtToken_1 = require("../helpers/getJwtToken");
 const cookieToken = (user, res) => {
     const token = (0, getJwtToken_1.getJwtToken)(user.id);
+    const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+    };
+    user.password = undefined;
 };
--- src/utils/cookieToken.ts
+++ src/utils/cookieToken.ts
@@ -1,6 +1,13 @@
-import { getJwtToken } from "../helpers/getJwtToken";
-import Response from "express"
+import { getJwtToken } from '../helpers/getJwtToken';
+import Response from 'express';
 
-const cookieToken = (user : any, res : Response) => {
-    const token : string = getJwtToken(user.id)
-}
+const cookieToken = (user: any, res: Response) => {
+  const token: string = getJwtToken(user.id);
+
+  const options = {
+    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  user.password = undefined;
+};
