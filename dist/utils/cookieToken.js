325db816d28de62284dad91929479033ae23e8f5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-17 23:07:20 +0100

    Code working as expected

--- dist/utils/cookieToken.js
+++ dist/utils/cookieToken.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const getJwtToken_1 = require("../helpers/getJwtToken");
 const cookieToken = (user, res) => {
+    const token = (0, getJwtToken_1.getJwtToken)(user.id);
 };
