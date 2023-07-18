76bb262ad0d30b05a0573f5f3ab1f6fc220534e1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-18 21:46:38 +0100

    working on signup controller

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -9,17 +9,18 @@ export const signUp = async (
   next: NextFunction
 ) => {
   try {
-      const { name, email, password } = req.body;
-      if (!name || !email || !password) {
-          throw new Error("Please provide all required fields")
-      }
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      throw new Error('Please provide all required fields');
+    }
 
-      const user = await prisma.user.create({
-          data: {
-              name,
-              email,
-              password
-          }
-      })
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    cookieToken(user, res);
   } catch (error) {}
 };
